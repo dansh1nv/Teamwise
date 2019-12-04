1c6e255b1293e1817afca41c23ece78dc4076482
Vladimir Danshin <dansh1nv@yandex.ru>
2019-12-05 02:36:54 +0300

    Created design Profile Fragment

--- app/src/main/java/com/vladimir/teamwise/HostActivity.kt
+++ app/src/main/java/com/vladimir/teamwise/HostActivity.kt
@@ -3,29 +3,43 @@ package com.vladimir.teamwise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_host.*
 
 class HostActivity : AppCompatActivity() {
 
-    private lateinit var navController : NavController
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
         setSupportActionBar(toolbar)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        
 
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
         bottom_nav.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        AppBarConfiguration(
+            setOf(
+                R.id.additionFragment,
+                R.id.chatFragment,
+                R.id.informationFragment,
+                R.id.profileFragment,
+                R.id.searchFragment
+            )
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
 
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, null)
-    }
 }
