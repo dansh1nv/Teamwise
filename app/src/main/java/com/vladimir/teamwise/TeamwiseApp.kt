9616a28f751bf3007c218455ec0d5895982f52f9
Vladimir <vladimir.danshin@krit.pro>
2020-08-02 14:45:54 +0300

    Some fix

--- app/src/main/java/com/vladimir/teamwise/TeamwiseApp.kt
+++ app/src/main/java/com/vladimir/teamwise/TeamwiseApp.kt
@@ -1,12 +1,13 @@
 package com.vladimir.teamwise
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.mapbox.mapboxsdk.Mapbox
 
 class TeamwiseApp : Application(){
     override fun onCreate() {
         super.onCreate()
-
+        Stetho.initializeWithDefaults(this)
         Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
     }
 }
--- app/src/main/java/com/vladimir/teamwise/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/vladimir/teamwise/ui/auth/AuthFragment.kt
@@ -1,20 +1,15 @@
 package com.vladimir.teamwise.ui.auth
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
 import com.vladimir.teamwise.R
+import kotlinx.android.synthetic.main.auth_fragment.*
 import kotlinx.android.synthetic.main.host_activity.*
-import kotlinx.android.synthetic.main.host_activity.view.*
 
 class AuthFragment : Fragment() {
 
@@ -33,7 +28,7 @@ class AuthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.auth_button).setOnClickListener {
+        auth_button.setOnClickListener {
             val informationStringArg = "information"
             val action = AuthFragmentDirections.nextAction(informationStringArg)
             findNavController().navigate(action)
